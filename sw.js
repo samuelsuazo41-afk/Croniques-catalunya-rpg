856f4378285b3788650497bcbf609bed896b49fc
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-26 21:28:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,70 @@
+// Cambia esto cada vez que subas cambios. Formato: YYYYMMDD-HHMM
+const CACHE_VERSION = '20251004-1430';
+const CACHE_NAME = `croniques-catalunya-rpg-${CACHE_VERSION}`;
 
+const urlsToCache = [
+  './',
+  './index.html',
+  './main.js',
+  './styles.css',
+  './manifest.json',
+  
+  // Data del juego
+  './data/items.json',
+  './data/capitols.json',
+  './data/capitol_01_bcn_born.json',
+  './data/capitol_02_girona.json',
+  './data/capitol_03_fires_valencia.json',
+  './data/ruta_rave_port_olympic.json',
+  './data/ruta_girona_muralla_viva.json',
+  './data/ruta_valencia_ciutat_vella.json',
+  './data/biblioteca_emojis.json',
+  './data/botiga_emojis.json',
+  './data/categories_emoji.json',
+  './data/minijoc_frases.json',
+  
+  // Leyendas
+  './data/llegendes_barcelona.json',
+  './data/llegendes_girona.json',
+  './data/llegendes_valencia.json',
+  
+  // Assets
+  './camisa_cenguera.png',
+  './ram_roses_girona.png',
+  './fuet_fires.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.log('Cache failed:', err))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName.startsWith('croniques-catalunya-rpg-') && cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
